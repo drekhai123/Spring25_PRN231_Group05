d81fab2d0fdb90a1eb78f4e02d806017d4bc46a8
84796578ran <108784652+84796578ran@users.noreply.github.com>
2025-03-13 15:42:58 +0700

    add api with odata

---
+++ Spring25_PRN231_Group05/FlowerFarmTaskManagementSystem.APIWithOdata/Controllers/ProductFieldsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FlowerFarmTaskManagementSystem.BusinessLogic.IService;
+using FlowerFarmTaskManagementSystem.BusinessObject.DTO;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace FlowerFarmTaskManagementSystem.APIWithOdata.Controllers
+{
+    [Route("odata/[controller]")]
+    [ApiController]
+    public class ProductFieldsController : ControllerBase
+    {
+        private readonly IProductFieldService _productFieldService;
+
+        public ProductFieldsController(IProductFieldService productFieldService)
+        {
+            _productFieldService = productFieldService;
+        }
+
+        [HttpGet("get-all-productField")]
+        [EnableQuery]
+        public async Task<ActionResult<IEnumerable<ProductFieldRequest>>> GetAllProductFields()
+        {
+            var productFields = await _productFieldService.GetAllProductFieldsAsync();
+            return Ok(productFields);
+        }
+
+        [HttpGet("get-by-id")]
+        public async Task<ActionResult<ProductFieldRequest>> GetProductFieldById(Guid id)
+        {
+            try
+            {
+                var productField = await _productFieldService.GetProductFieldByIdAsync(id);
+                return Ok(productField);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpDelete("delete-by-id")]
+        public async Task<ActionResult<bool>> DeleteProductFieldAsync(Guid id)
+        {
+            try
+            {
+                await _productFieldService.DeleteProductFieldsAsync(id);
+                return NoContent();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { Message = ex.Message });
+            }
+        }
+    }
+}
