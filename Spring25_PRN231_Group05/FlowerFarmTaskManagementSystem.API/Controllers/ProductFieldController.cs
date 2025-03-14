a2933748a4af2e7f91549110bba1ec84a379de6e
Tran Minh Quan <97839306+micharel09@users.noreply.github.com>
2025-03-14 10:51:08 +0700

    fix conflict ProductField

--- Spring25_PRN231_Group05/FlowerFarmTaskManagementSystem.API/Controllers/ProductFieldController.cs
+++ Spring25_PRN231_Group05/FlowerFarmTaskManagementSystem.API/Controllers/ProductFieldController.cs
@@ -40,71 +40,71 @@ namespace FlowerFarmTaskManagementSystem.API.Controllers
         }
 
         // POST: odata/ProductField/create-product-field
-        [HttpPost("create-product-field")]
-        public async Task<ActionResult<ProductFieldDTO>> CreateProductField([FromBody] ProductFieldCreateDTO productFieldCreateDTO)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //[HttpPost("create-product-field")]
+        //public async Task<ActionResult<ProductFieldDTO>> CreateProductField([FromBody] ProductFieldCreateDTO productFieldCreateDTO)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            try
-            {
-                var productField = await _productFieldService.AddProductFieldAsync(productFieldCreateDTO);
-                return CreatedAtAction(nameof(GetProductFieldById), new { id = productField.ProductFieldId }, productField);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                return NotFound(new { Message = ex.Message });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { Message = ex.Message });
-            }
-        }
+        //    try
+        //    {
+        //        var productField = await _productFieldService.AddProductFieldAsync(productFieldCreateDTO);
+        //        return CreatedAtAction(nameof(GetProductFieldById), new { id = productField.ProductFieldId }, productField);
+        //    }
+        //    catch (KeyNotFoundException ex)
+        //    {
+        //        return NotFound(new { Message = ex.Message });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(new { Message = ex.Message });
+        //    }
+        //}
 
-        // PUT: odata/ProductField/update-product-field
-        [HttpPut("update-product-field")]
-        public async Task<ActionResult<ProductFieldDTO>> UpdateProductField(Guid id, [FromBody] ProductFieldUpdateDTO productFieldUpdateDTO)
-        {
-            if (id != productFieldUpdateDTO.ProductFieldId)
-            {
-                return BadRequest(new { Message = "ProductField ID mismatch." });
-            }
+        //// PUT: odata/ProductField/update-product-field
+        //[HttpPut("update-product-field")]
+        //public async Task<ActionResult<ProductFieldDTO>> UpdateProductField(Guid id, [FromBody] ProductFieldUpdateDTO productFieldUpdateDTO)
+        //{
+        //    if (id != productFieldUpdateDTO.ProductFieldId)
+        //    {
+        //        return BadRequest(new { Message = "ProductField ID mismatch." });
+        //    }
 
-            try
-            {
-                var updatedProductField = await _productFieldService.UpdateProductFieldAsync(id, productFieldUpdateDTO);
-                return Ok(updatedProductField);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                return NotFound(new { Message = ex.Message });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { Message = ex.Message });
-            }
-        }
+        //    try
+        //    {
+        //        var updatedProductField = await _productFieldService.UpdateProductFieldAsync(id, productFieldUpdateDTO);
+        //        return Ok(updatedProductField);
+        //    }
+        //    catch (KeyNotFoundException ex)
+        //    {
+        //        return NotFound(new { Message = ex.Message });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(new { Message = ex.Message });
+        //    }
+        //}
 
-        // DELETE: odata/ProductField/delete-product-field
-        [HttpDelete("delete-product-field")]
-        public async Task<ActionResult> DeleteProductField(Guid id)
-        {
-            try
-            {
-                await _productFieldService.DeleteProductFieldAsync(id);
-                return NoContent();
-            }
-            catch (KeyNotFoundException ex)
-            {
-                return NotFound(new { Message = ex.Message });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { Message = ex.Message });
-            }
-        }
+        //// DELETE: odata/ProductField/delete-product-field
+        //[HttpDelete("delete-product-field")]
+        //public async Task<ActionResult> DeleteProductField(Guid id)
+        //{
+        //    try
+        //    {
+        //        await _productFieldService.DeleteProductFieldAsync(id);
+        //        return NoContent();
+        //    }
+        //    catch (KeyNotFoundException ex)
+        //    {
+        //        return NotFound(new { Message = ex.Message });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(new { Message = ex.Message });
+        //    }
+        //}
 
         [HttpDelete("delete-by-id")]
         public async Task<ActionResult<bool>> DeleteProductFieldAsync(Guid id)
